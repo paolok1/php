4a3f1be65559d97fa7972365da5176e4fac95733
paolok1 <gpaolok1@gmail.com>
2025-02-19 19:16:49 +0100

    selfwork2_php

---
+++ php_02_paolo_giuliani.php
@@ -0,0 +1,11 @@
+<?php
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = $text2;
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo($text1 ." ". $text2 . " ". $text3 ." ". $text4 ." ". $text5 . " ". $text6 ." ". $text7 ." ". $text8);
