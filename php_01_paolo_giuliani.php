e708324ec2df88b29f850d4ca3e2c399d37842e8
paolok1 <gpaolok1@gmail.com>
2025-02-19 18:55:53 +0100

    selfwork1_php

---
+++ php_01_paolo_giuliani.php
@@ -0,0 +1,19 @@
+<?php
+// $integer = 10;
+// $float = 10.5;
+// $string ="Stringa";
+// $booleano = true;
+// var_dump($integer);
+// var_dump($float);
+// var_dump($string);
+// var_dump($booleano);
+
+
+// const INTERO = 10;
+// const VIRGOLA = 10.5;
+// const STRINGA ="Stringa";
+// const BOOLEANO = true;
+// var_dump(INTERO);
+// var_dump(VIRGOLA);
+// var_dump(STRINGA);
+// var_dump(BOOLEANO);
