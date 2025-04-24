fd6ad52fd6b7b965e5c022e0a5bb6c54348f4afa
paolok1 <gpaolok1@gmail.com>
2025-04-24 18:05:44 +0200

    selfwork php oop5

---
+++ class.php
@@ -0,0 +1,50 @@
+<?php
+
+abstract class Category{
+    abstract public function GetMyCategory();
+}
+
+class Attuality extends Category{
+    public function GetMyCategory(){
+        echo "Attualità \n";
+    }
+}
+
+
+class Sport extends Category{
+    public function GetMyCategory(){
+        echo "Sport \n";
+    }
+}
+
+    
+
+class Gossip extends Category{
+    public function GetMyCategory(){
+        echo "Gossip \n";
+    }
+}
+
+class History extends Category{
+    public function GetMyCategory(){
+        echo "Storia \n";
+    }
+}
+$attualita = new Attuality();
+$attualita->GetMyCategory();
+$gossip = new Gossip();
+$gossip->GetMyCategory();
+$sport = new Sport();
+$sport->GetMyCategory();
+$storia = new History();
+$storia->GetMyCategory();
+
+
+
+
+
+
+
+
+
+
